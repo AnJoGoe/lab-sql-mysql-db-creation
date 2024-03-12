9022851c492665d5189e2de572a7bd65106f19a6
AnJoGoe <anjohan.goe@gmail.com>
2024-03-12 09:50:17 +0100

    Update comment

--- create.sql
+++ create.sql
@@ -1,21 +1,21 @@
 
-#### Challenge 2 - Create the Database and Tables ####
+-- ============ Challenge 2 - Create the Database and Tables ============   
 
-## Create a MySQL database  
+-- ======  Create a MySQL database  
 CREATE DATABASE IF NOT EXISTS lab_mysql;
 USE lab_mysql;
 
 
-## Write SQL queries to create the tables
+-- ======  Write SQL queries to create the tables
 
-# Drop Tables
+--  Drop Tables
 DROP TABLE IF EXISTS invoices;
 DROP TABLE IF EXISTS cars;
 DROP TABLE IF EXISTS customers;
 DROP TABLE IF EXISTS salespersons ;
 
 
-# CREATE CARS TABLE
+--  CREATE CARS TABLE
 CREATE TABLE cars (
 car_id INT AUTO_INCREMENT PRIMARY KEY,
 vin VARCHAR(45) NOT NULL,
@@ -26,7 +26,7 @@ color VARCHAR(45)
 );
 
 
-# CREATE CUSTOMERS TABLE
+--  CREATE CUSTOMERS TABLE
 CREATE TABLE customers (
 cust_id INT AUTO_INCREMENT PRIMARY KEY,
 cust_name VARCHAR(80) NOT NULL,
@@ -40,14 +40,14 @@ cust_postal INT
 );
 
 
-# CREATE SALESPERSON TABLE
+--  CREATE SALESPERSON TABLE
 CREATE TABLE salespersons (
 sales_id INT AUTO_INCREMENT PRIMARY KEY,
 salescar_idcar_id_name VARCHAR(80) NOT NULL,
 sales_store VARCHAR(25) NOT NULL
 );
 
-# CREATE INVOICES TABLE
+--  CREATE INVOICES TABLE
 CREATE TABLE invoices (
 invoice_id INT AUTO_INCREMENT PRIMARY KEY,
 date DATE NOT NULL,
@@ -58,3 +58,5 @@ FOREIGN KEY (car_id) REFERENCES cars(car_id),
 FOREIGN KEY (cust_id) REFERENCES customers(cust_id),
 FOREIGN KEY (sales_id) REFERENCES salespersons(sales_id)
 );
+
+-- END OF Script
